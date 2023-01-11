01439575102539decdc39c386b51ab2ca3fcd75c
Mohini <mohini5395@gmail.com>
2023-01-11 23:41:47 +0530

    done

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
+let input = document.getElementById('newTodoInput');
+let addBtn = document.getElementById('addTodoBtn');
+let orderedList = document.getElementById('todoList');
 
+function addTodoItem() {
+    if(input.value !== ""){
+    let li = document.createElement('li');
+ li.textContent = input.value;
+
+//  orderedList.classList.add('center');
+ orderedList.appendChild(li);
+ input.value = "";
+}
+}
+addBtn.addEventListener('click', addTodoItem);
